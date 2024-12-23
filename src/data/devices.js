59fd84670924c643f81281c7eaceed3ad5d38864
Julia <uug11@mail.ru>
2024-12-23 16:15:15 +0400

    added all devices pages

---
+++ src/data/devices.js
@@ -0,0 +1,33 @@
+export default {
+  products:[
+    {
+      code: 'altruist',
+      title: 'Outdoor sensor “Altruist“',
+      text: `A smart sensor that collects environmental data - noise, dust, and temperature, and stores them on a decentralized <a aria-label="see decentralized sensor map" href="https://sensors.social/" target="_blank">sensor map</a>.`,
+      cases: 'Become a provider of environmental data to your friends and neighbors in the area. Access truly decentralized measurements of air quality, noise, temperature, and other environmental conditions on the sensor map.',
+      casesImages: ['altruist-cases-1.webp', 'altruist-cases-2.webp', 'altruist-cases-3.webp', 'altruist-cases-4.webp'],
+      titleLink: '/devices/altruist'
+    },
+    {
+      code: 'server',
+      title: 'Home server with Web3 cloud',
+      text: 'The most open-source smart home server, powered by RISC-V, with a Web3 cloud replacing Google servers under the hood.',
+      cases: 'Smart WI-FI MQTT devices with open-source firmware Tasmota on the board fully compatible with Home Assistant.',
+      casesImages: ['risc-v-cases-1.webp', 'risc-v-cases-2.webp', 'risc-v-cases-3.webp', 'risc-v-cases-4.webp'],
+      titleLink: '/devices/risc-v'
+    },
+    {
+      code: 'hikikomori',
+      title: 'Smart Tamagochi “Hikikomori“',
+      text: 'Finally, a smart Tamagotchi that’s more than just a game! A smart wearable station that checks the status of sensors from your home or clothing.',
+      cases: 'Connect your smart home and smart clothing to the Hikikomori to monitor sensor data—right at your fingertips. Literally.',
+      titleLink: '/devices/hikikomori'
+    },
+    {
+      code: 'energy-monitor',
+      title: 'Energy monitor with USB-C',
+      text: 'A non-invasive device for installation in electrical panels with DIN rail mounting. Built on the Espressif ESP32C6 chip with open RISC-V architecture.',
+      titleLink: '/devices/energy-monitor'
+    },
+  ],
+}
