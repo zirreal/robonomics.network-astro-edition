9aa7c0dfc6c97bd76bef46dcd2c0b1ec3dd27117
Julia <uug11@mail.ru>
2025-03-11 19:13:07 +0400

    started adding cases pages

---
+++ src/data/cases.js
@@ -0,0 +1,347 @@
+export default {
+  cases: [
+    {
+      id: 0,
+      title: $tr('Fronius Solar.web integration with Home Assistant'),
+      description: $tr('Collaboration with real estate developer on Cyprus for tracking the daily distribution, consumption and offsetting any excess carbon footprint.'),
+      path: 'cases/fronius-solar-web',
+      cover_image: 'fronius-solar-web.webp',
+      metaImg: '/website_cover_fronius_solar_web.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2022-12-31',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2023-07-14',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['smart home', 'carbon footprint offsetting'],
+      done: true
+    },
+    {
+      id: 1,
+      title: $tr('Robonomics Middleware Framework for ROS 2'),
+      description: $tr('One of our goals in developing this case is to turn Robonomics into ROS 2 Middleware.This will allow us to use Robonomics on Substrate to create an dapp that can run ROS 2 right from the browser!'),
+      path: 'cases/rust-for-ros2',
+      cover_image: 'rust-ros2.webp',
+      metaImg: '/website_cover_rust_ros2.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'proceeding',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2022-08-08',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2023-07-12',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['robotics'],
+      done: true
+    },
+    {
+      id: 2,
+      title: $tr('Eisenkoch, the robot chef'),
+      description: $tr('Real case fot the robot equipped with two waffle irons and baking 6 waffles at the same time in 5-7 minutes.'),
+      path: 'cases/eisenkoch',
+      cover_image: 'eisenkoch.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2022-03-27',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2023-03-27',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['robotics', 'polkadot ecosystem', 'smart service'],
+      done: false
+    },
+    {
+      id: 3,
+      title: $tr('Blockchain-powered smart coffee machine'),
+      description: $tr('Robonomics coffee - is a smart coffee machine integrated in Robonomics Network. This project aims to show Robonomics potential in the IoT sphere by a real-world example.'),
+      path: 'cases/blockchain-coffee-machine',
+      cover_image: 'blockchain-coffee.webp',
+      metaImg: '/website_cover_coffee-machine.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2022-02-11',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2023-02-11',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['smart service', 'polkadot ecosystem'],
+      done: true
+    },
+    {
+      id: 4,
+      title: $tr('Water environmental inspector'),
+      description: '',
+      path: 'cases/water-inspector',
+      cover_image: 'water-inspector.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2022-01-16',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2023-01-16',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['robotics', 'polkadot ecosystem', 'sensors', 'data storage'],
+      done: false
+    },
+    {
+      id: 5,
+      title: $tr('Air monitoring network'),
+      description: '',
+      path: 'cases/air-monitoring-network',
+      cover_image: 'air-monitoring.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'proceeding',
+      // created field needs for "sort by date" filter (for earliest option)
+      // created: '2022-01-03'
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2023-01-03',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['polkadot ecosystem', 'sensors', 'data storage'],
+      done: false
+    },
+    {
+      id: 6,
+      title: $tr('Production quality control platform “Feecc”'),
+      description: '',
+      path: 'cases/quality-control-platform-feecc',
+      cover_image: 'feecc.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'proceeding',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2021-05-06',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2022-05-06',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['polkadot ecosystem', 'data storage', 'smart service'],
+      done: false
+    },
+    {
+      id: 7,
+      title: $tr('Backup and archiving service for robotics companies'),
+      description: $tr('In 2022, the Robonomics team began collaborating with other parachain teams within the Polkadot ecosystem to leverage the unique strengths of this decentralized network. This collaboration resulted in the launch of a Backup and Data Archiving Service designed specifically for robotics companies.'),
+      path: 'cases/service-for-robotics-companies',
+      cover_image: 'robotic-companies.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2021-04-18',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2022-04-18',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['robotics', 'polkadot ecosystem', 'data storage'],
+      done: true
+    },
+    {
+      id: 8,
+      title: $tr('Public lab with Boston Dynamics Spot'),
+      description: '',
+      path: 'cases/quality-control-platform-feecc',
+      cover_image: 'spot.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'proceeding',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2021-11-05',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2022-11-05',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['robotics', 'polkadot ecosystem', 'ethereum', 'smart service'],
+      done: false
+    },
+    {
+      id: 9,
+      title: $tr('Autonomous telescope service'),
+      description: '',
+      path: 'cases/quality-control-platform-feecc',
+      cover_image: 'telescope.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2022-06-02',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2022-12-17',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['robotics', 'polkadot ecosystem', 'smart service'],
+      done: false
+    },
+    {
+      id: 10,
+      title: $tr('Smart home secure remote control and backup service'),
+      description: '',
+      path: 'cases/quality-control-platform-feecc',
+      cover_image: 'smart-home.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'proceeding',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2021-01-21',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2023-02-15',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['polkadot ecosystem', 'smart home', 'data storage'],
+      done: false
+    },
+    {
+      id: 11,
+      title: $tr('Blockchain as quality control system for chemistry'),
+      description: '',
+      path: 'cases/quality-control-platform-feecc',
+      cover_image: 'blockchain-chemistry.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2020-12-09',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2022-10-15',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['ethereum', 'smart service', 'data storage'],
+      done: false
+    },
+    {
+      id: 12,
+      title: $tr('Real CO2 emissions reduction with blockchain-proven certificates'),
+      description: '',
+      path: 'cases/quality-control-platform-feecc',
+      cover_image: 'emissions.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2020-08-22',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2021-04-11',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['ethereum', 'smart service', 'carbon footprint offsetting'],
+      done: false
+    },
+    {
+      id: 13,
+      title: $tr('Robot-artist Gaka-Chu'),
+      description: '',
+      path: 'cases/quality-control-platform-feecc',
+      cover_image: 'gakachu.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2020-03-19',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2021-05-06',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['ethereum', 'smart service'],
+      done: false
+    },
+    {
+      id: 14,
+      title: $tr('Digital markets for robots'),
+      description: $tr('The development of robotics and automated systems during the Fourth Industrial Revolution will require robots to have the capability to order services for their own maintenance.'),
+      path: 'cases/digital-markets-for-robots',
+      cover_image: 'digital-markets-for-robots.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2020-05-15',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2021-04-08',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['robotics', 'ethereum', 'polkadot ecosystem', 'smart service'],
+      done: true
+    },
+    {
+      id: 15,
+      title: $tr('Demo “Get the weather of Fuji mountain”'),
+      description: 'Send a request message and get the result from the autonomous agent. The resulting file is saved to IPFS and the resultant message requires a signature with a private key.',
+      path: 'cases/the-weather-of-fuji',
+      cover_image: 'fuji.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2020-02-17',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2021-03-17',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['ethereum', 'smart service'],
+      done: true
+    },
+    {
+      id: 16,
+      title: $tr('Tokenization of Data of IOT devices'),
+      description: $tr('The tokenization of data from IoT (Internet of Things) devices represents a groundbreaking development in the digital economy.'),
+      path: 'cases/tokenization',
+      cover_image: 'tokenization.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2019-05-15',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2020-04-18',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['ethereum'],
+      done: true
+    },
+    {
+      id: 17,
+      title: $tr('Industrial zone management with capital'),
+      description: $tr('This case explores the role of smart factories in responding to capital shifts in modern markets. As part of the Industry 4.0 transformation, it introduces a decentralized communication protocol using cyber-physical systems to improve economic coordination among industrial agents.'),
+      path: 'cases/industrial-zone-management-with-capital',
+      cover_image: 'industrial-zone-management.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2020-05-15',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2021-07-08',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['robotics', 'ethereum', 'polkadot ecosystem', 'smart service'],
+      done: true
+    },
+    {
+      id: 18,
+      title: $tr('Mars colonization, impossible without kusama'),
+      description: $tr("This R&D case explores the role of Kusama network's decentralized infrastructure in supporting interplanetary missions, particularly Mars colonization."),
+      path: 'cases/mars-colonization',
+      cover_image: 'mars-colonization.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2020-05-15',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2021-07-08',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['robotics', 'polkadot ecosystem', 'data storage', 'sensors'],
+      done: true
+    },
+    {
+      id: 18,
+      title: $tr('A decentralized system for negotiating road space'),
+      description: $tr('Our goal was to develop a decentralized system for negotiating road space where autonomous vehicles can pay for routes and permissions.'),
+      path: 'cases/negotiating-road-space',
+      cover_image: 'road-space.webp',
+      // progress field needs for "sort by progress" filter
+      progress: 'complete',
+      // created field needs for "sort by date" filter (for earliest option)
+      created: '2021-02-11',
+      // updated field needs for "sort by date" filter (for recent option)
+      updated: '2021-11-05',
+      // tags field needs for "sort by interest" filter
+      // use lowerCase
+      tags: ['robotics', 'ethereum', 'polkadot ecosystem', 'smart service'],
+      done: true
+    }
+  ]
+}
