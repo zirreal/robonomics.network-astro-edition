0a9df72b90bdc6c0b64589fdc34fdd42a8cc9b9a
Julia <uug11@mail.ru>
2025-03-31 19:32:08 +0400

    some layout fixes

--- src/content.config.js
+++ src/content.config.js
@@ -7,14 +7,6 @@ import { glob } from 'astro/loaders';
 // 3. Define your collection(s)
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/blog" }),
-  // schema: z.object({
-  //   title: z.string(),
-  //   date: z.date(),
-  //   description: z.string(),
-  //   tags: z.array(z.string()),
-  //   cover_image: z.string(),
-  //   locale: z.string(),
-  // })
 });
 
 // 4. Export a single `collections` object to register your collection(s)
